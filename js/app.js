cca8a0182db097df8be336b2a4f17cae8dc009ec
danielgallo <daniel.gallo@gmail.com>
2024-04-21 20:05:09 +0100

    Fix power flow line (battery to house)

--- js/app.js
+++ js/app.js
@@ -240,7 +240,9 @@ class App {
                     // the "Solar_to_House" can be zero (along with the other flows), so ensure
                     // the "solar to house" flow line is still rendered.
                     data.Solar_to_House = 10;
-                } else if (data.Battery_to_House === 0 && data.Solar_to_House === 0 && data.Grid_to_House === 0 && data.Battery_to_Grid > 10) {
+                }
+
+                if (data.Battery_to_House === 0 && data.Grid_to_House === 0 && data.Battery_to_Grid > 10) {
                     // If multiple inverters are exporting, ensure the flow line from the battery to the
                     // house is still flowing.
                     data.Battery_to_House = 10;
